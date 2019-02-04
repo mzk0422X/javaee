011f3b1ed4be12cb71f46a776b34000daa312cad
Administrator <Administrator@192.168.56.1>
2019-02-04 16:28:37 +0800

    HelloWorld的第二次修改

--- java程序/javase/src/com/mzk/Helloworld/HelloWorld.java
+++ java程序/javase/src/com/mzk/Helloworld/HelloWorld.java
@@ -3,6 +3,6 @@ package com.mzk.Helloworld;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("HelloWrold!!");
+		System.out.println("HelloWrold!!第二次修改");
 	}
 }
